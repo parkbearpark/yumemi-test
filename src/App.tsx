417aecf0458fdaa4dcfc84444e2b58ad4823aadd
yuma <yumai6205@gmail.com>
2023-08-06 18:02:35 +0900

    チェックしたら取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { getPrefectures } from './api'
-import { Prefecture } from './types'
+import { getPrefectures, getPopulation } from './api'
+import { Population, Prefecture } from './types'
 import './App.css'
 
 function App() {
@@ -14,13 +14,31 @@ function App() {
     })
   }, [])
 
+  const handleCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const target = e.target
+    const value = parseInt(target.value)
+    const prefIndex = prefectures.findIndex((prefecture) => prefecture.prefCode === value)
+    const prefName = prefectures[prefIndex].prefName
+
+    if (target.checked) {
+      getPopulation(value).then((res) => {
+        const population = res[0].data as Population[]
+        const color = `#${Math.floor(Math.random() * 16777215).toString(16)}`
+
+        })
+      }).catch((err) => {
+        console.log(err)
+      })
+    }
+  }
+
   return (
     <div className="App">
       <div className="checkbox-container">
         {prefectures.map((prefecture) => {
           return (
             <div key={prefecture.prefCode}>
-              <input type="checkbox" id={`${prefecture.prefCode}`} name={prefecture.prefName} value={prefecture.prefCode} />
+              <input type="checkbox" id={`${prefecture.prefCode}`} name={prefecture.prefName} value={prefecture.prefCode} onChange={handleCheck} />
               <label htmlFor={`${prefecture.prefCode}`}>{prefecture.prefName}</label>
             </div>
           )
