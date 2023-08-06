f5ac90d5855f6cdc4a64b624e653cf5d4df3b3d0
yuma <yumai6205@gmail.com>
2023-08-06 18:03:17 +0900

    グラフの描画部分

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import 'chart.js/auto'
+import { ChartData } from 'chart.js'
+import { Line } from 'react-chartjs-2'
 import { getPrefectures, getPopulation } from './api'
 import { Population, Prefecture } from './types'
 import './App.css'
 
 function App() {
   const [prefectures, setPrefectures] = React.useState<Prefecture[]>([])
+  const [chartData, setChartData] = React.useState<ChartData<'line', number[], string>>({
+    labels: [],
+    datasets: [],
+  })
 
   React.useEffect(() => {
     getPrefectures().then((res) => {
@@ -25,9 +32,28 @@ function App() {
         const population = res[0].data as Population[]
         const color = `#${Math.floor(Math.random() * 16777215).toString(16)}`
 
+        setChartData({
+          labels: population.map((population) => `${population.year}`),
+          datasets: [...chartData.datasets,
+             {
+              label: prefName,
+              data: population.map((population) => population.value),
+              type: 'line',
+              borderColor: color,
+              backgroundColor: color,
+              fill: false,
+            }
+          ]
         })
       }).catch((err) => {
         console.log(err)
+      })
+    } else {
+      setChartData({
+        labels: chartData.labels,
+        datasets: chartData.datasets.filter((dataset) => {
+          return dataset.label !== prefName
+        })
       })
     }
   }
@@ -45,7 +71,7 @@ function App() {
         })}
       </div>
       <div className="graph-container">
-
+        {chartData !== null && <Line data={chartData} />}
       </div>
     </div>
   )
